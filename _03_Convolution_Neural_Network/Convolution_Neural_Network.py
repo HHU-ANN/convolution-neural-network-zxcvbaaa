9966b4829bbdad7ab2ece34af14fca56e2e1a220
zxcvbaaa <1599356984@qq.com>
2023-06-24 16:54:27 +0800

    1

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,4 +1,4 @@
-# 在该文件NeuralNetwork类中定义你的模型 
+# 在该文件NeuralNetwork类中定义你的模型
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
 import os
@@ -12,14 +12,31 @@ import torch.optim as optim
 import torchvision
 
 from torch.utils.data import DataLoader
-    
 
 class NeuralNetwork(nn.Module):
-    pass
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.resnet = torchvision.models.resnet34(pretrained=False)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, 100)
+
+    def forward(self, x):
+        return self.resnet(x)
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
+    transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+    transform_test = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
@@ -30,8 +47,45 @@ def main():
     model = NeuralNetwork() # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    #测试测试测试测试测试
+    num_epochs = 10
+    learning_rate = 0.1
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+
+    dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
+
+    model = model.to(device)
+
+    for epoch in range(num_epochs):
+        train_loss = 0
+        train_acc = 0
+        model.train()  # set the model to training mode
+
+        for i, (images, labels) in enumerate(data_loader_train):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * images.size(0)
+            _, predicted = torch.max(outputs.data, 1)
+            train_acc += (predicted == labels).sum().item()
+
+        train_loss = train_loss / len(dataset_train)
+        train_acc = train_acc / len(dataset_train)
+
+        print('Epoch {}, Train Loss: {:.4f}, Train Acc: {:.4f}'.format(epoch + 1, train_loss, train_acc))
 
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
-    
+
+
+
+
+
