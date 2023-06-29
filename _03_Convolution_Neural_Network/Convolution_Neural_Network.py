3c4b08eb4a6ffbe89fc24e2d6723031e9014eaf2
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-06-29 14:38:46 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -3,103 +3,106 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-
 from torch.utils.data import DataLoader
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, num_classes=10):
-        super(NeuralNetwork, self).__init__()
 
-        # 定义卷积层和全连接层
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.conv4 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.conv5 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.relu5 = nn.ReLU(inplace=True)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+        self.relu6 = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout()
+        self.fc2 = nn.Linear(4096, 4096)
+        self.relu7 = nn.ReLU(inplace=True)
+        self.dropout2 = nn.Dropout()
+        self.fc3 = nn.Linear(4096, 10)
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.conv4(x)
+        x = self.relu4(x)
+        x = self.conv5(x)
+        x = self.relu5(x)
+        if x.size(-1) >= 3 and x.size(-2) >= 3:  # Check if the size is large enough for max pooling
+            x = self.maxpool3(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu6(x)
+        x = self.dropout1(x)
         return x
 
 
 def read_data():
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=False,
-                                                 transform=torchvision.transforms.Compose([
-                                                     torchvision.transforms.Resize((227, 227)),
-                                                     torchvision.transforms.ToTensor()
-                                                 ]))
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
+                                                 transform=torchvision.transforms.ToTensor())
     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
-                                               transform=torchvision.transforms.Compose([
-                                                   torchvision.transforms.Resize((227, 227)),
-                                                   torchvision.transforms.ToTensor()
-                                               ]))
-    data_loader_train = DataLoader(dataset_train, batch_size=32, shuffle=True)
-    data_loader_val = DataLoader(dataset_val, batch_size=32, shuffle=False)
+                                               transform=torchvision.transforms.ToTensor())
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    return dataset_train, dataset_val, data_loader_train, data_loader_val
 
-    return dataset_train, dataset_val, data_loader_train, data_loader_val # Assuming there are 10 classes
 
-def main():
-    model = NeuralNetwork()
-    dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
-    num_classes = 10
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+def train(model, data_loader_train, data_loader_val):
     criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(10):
+    for epoch in range(1):
+        running_loss = 0.0
         model.train()
         for images, labels in data_loader_train:
             optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+            output = model(images)
+            loss = criterion(output, labels)
             loss.backward()
             optimizer.step()
+            running_loss += loss.item()
+        print(f"Epoch {epoch + 1}, Training Loss: {running_loss / len(data_loader_train)}")
 
         model.eval()
         correct = 0
         total = 0
         with torch.no_grad():
             for images, labels in data_loader_val:
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
+                output = model(images)
+                _, predicted = torch.max(output.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
+        accuracy = 100 * correct / total
+        print(f"Accuracy on validation set: {accuracy}%")
 
-        accuracy = correct / total
-        print(f"Epoch {epoch+1}/{10} | Accuracy: {accuracy:.2%}")
+    return model
 
-    # 保存模型参数
-    # dir_path = '../pth'
-    # os.makedirs(dir_path, exist_ok=True)
-    #
-    # torch.save(model.state_dict(), '../pth/model.pth')
-    #
-    # current_dir = os.path.dirname(os.path.abspath(__file__))
-    # parent_dir = os.path.dirname(current_dir)
-    # model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
 
+def main():
+    model = NeuralNetwork()
+    dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
+    model = train(model, data_loader_train, data_loader_val)
+    torch.save(model.state_dict(), '../pth/model.pth')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
 
 
