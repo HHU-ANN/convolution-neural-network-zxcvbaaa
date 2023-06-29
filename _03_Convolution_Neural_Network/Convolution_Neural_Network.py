fdd75c48489283dd89b06b585a83de2b202ead6f
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-06-29 19:57:25 +0800

    111

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -103,6 +103,7 @@ def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
+    
     return model
 
 
