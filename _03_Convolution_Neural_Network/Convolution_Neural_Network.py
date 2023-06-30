79f5f67688b8bc2c830a954a44fbcd00959d6c82
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-06-30 08:53:29 +0800

    222

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -4,55 +4,104 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torch.utils.data import DataLoader
+import torchvision.models as models
 
+# class NeuralNetwork(nn.Module):
+#     def __init__(self, num_classes=10):
+#         super(NeuralNetwork, self).__init__()
+#         self.resnet = models.resnet18(pretrained=True)
+#         # Freeze all the layers of the pre-trained ResNet
+#         for param in self.resnet.parameters():
+#             param.requires_grad = False
+#         # Replace the last fully connected layer to match the number of classes
+#         self.resnet.fc = nn.Linear(512, num_classes)
+#
+#     def forward(self, x):
+#         x = self.resnet(x)
+#         return x
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=5, padding=2)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.conv4 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.conv5 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
-        self.relu5 = nn.ReLU(inplace=True)
-        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
-        self.relu6 = nn.ReLU(inplace=True)
-        self.dropout1 = nn.Dropout()
-        self.fc2 = nn.Linear(4096, 4096)
-        self.relu7 = nn.ReLU(inplace=True)
-        self.dropout2 = nn.Dropout()
-        self.fc3 = nn.Linear(4096, 10)
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * out_channels)
+            )
+        else:
+            self.downsample = nn.Identity()
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.conv4(x)
-        x = self.relu4(x)
-        x = self.conv5(x)
-        x = self.relu5(x)
-        if x.size(-1) >= 3 and x.size(-2) >= 3:  # Check if the size is large enough for max pooling
-            x = self.maxpool3(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu6(x)
-        x = self.dropout1(x)
-        return x
+        identity = x
 
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += self.downsample(identity)
+        out = self.relu(out)
+
+        return out
+
+
+# ResNet Architecture
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, num_blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+
+        return out
+
+
+def NeuralNetwork(num_classes=10):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
 
 def read_data():
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
@@ -96,15 +145,15 @@ def train(model, data_loader_train, data_loader_val):
 
 
 def main():
-    model = NeuralNetwork()
+    model = NeuralNetwork(10)
     dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
     model = train(model, data_loader_train, data_loader_val)
     torch.save(model.state_dict(), '../pth/model.pth')
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
-    
     return model
+
 
 
 
