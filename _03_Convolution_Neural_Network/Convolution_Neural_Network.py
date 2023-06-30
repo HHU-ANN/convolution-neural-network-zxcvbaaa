438fc9ed1e6e31383f37798c410729d19c8edaba
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-06-30 18:16:44 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -4,104 +4,93 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torch.utils.data import DataLoader
-import torchvision.models as models
+from torch.cuda.amp import autocast, GradScaler
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, num_classes=10):
-        super(NeuralNetwork, self).__init__()
-        self.resnet = models.resnet18(pretrained=True)
-        # Freeze all the layers of the pre-trained ResNet
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-        # Replace the last fully connected layer to match the number of classes
-        self.resnet.fc = nn.Linear(512, num_classes)
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * out_channels)
+            )
+        else:
+            self.downsample = nn.Identity()
 
     def forward(self, x):
-        x = self.resnet(x)
-        return x
+        identity = x
 
-# class BasicBlock(nn.Module):
-#     expansion = 1
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-#     def __init__(self, in_channels, out_channels, stride=1):
-#         super(BasicBlock, self).__init__()
-#         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(out_channels)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(out_channels)
+        out = self.conv2(out)
+        out = self.bn2(out)
 
-#         if stride != 1 or in_channels != self.expansion * out_channels:
-#             self.downsample = nn.Sequential(
-#                 nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(self.expansion * out_channels)
-#             )
-#         else:
-#             self.downsample = nn.Identity()
+        out += self.downsample(identity)
+        out = self.relu(out)
 
-#     def forward(self, x):
-#         identity = x
-
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-
-#         out += self.downsample(identity)
-#         out = self.relu(out)
-
-#         return out
+        return out
 
 
-# # ResNet Architecture
-# class ResNet(nn.Module):
-#     def __init__(self, block, num_blocks, num_classes=10):
-#         super(ResNet, self).__init__()
-#         self.in_channels = 64
+# ResNet Architecture
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
 
-#         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-#         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
 
-#         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-#         self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
-#     def _make_layer(self, block, out_channels, num_blocks, stride):
-#         layers = []
-#         layers.append(block(self.in_channels, out_channels, stride))
-#         self.in_channels = out_channels * block.expansion
-#         for _ in range(1, num_blocks):
-#             layers.append(block(self.in_channels, out_channels))
-#         return nn.Sequential(*layers)
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, num_blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
 
-#     def forward(self, x):
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-#         out = self.maxpool(out)
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
 
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
 
-#         out = self.avgpool(out)
-#         out = torch.flatten(out, 1)
-#         out = self.fc(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
 
-#         return out
+        return out
 
 
-# def NeuralNetwork(num_classes=10):
-#     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
+def NeuralNetwork(num_classes=10):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = NeuralNetwork().to(device).half()
 
 def read_data():
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
@@ -113,45 +102,53 @@ def read_data():
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 
+
 def train(model, data_loader_train, data_loader_val):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    scaler = GradScaler()
 
     for epoch in range(10):
         running_loss = 0.0
         model.train()
         for images, labels in data_loader_train:
+            images = images.to(device).half()
+            labels = labels.to(device)
+
             optimizer.zero_grad()
-            output = model(images)
-            loss = criterion(output, labels)
-            loss.backward()
-            optimizer.step()
+
+            with autocast():
+                output = model(images)
+                loss = criterion(output, labels)
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
             running_loss += loss.item()
+
         print(f"Epoch {epoch + 1}, Training Loss: {running_loss / len(data_loader_train)}")
 
-        model.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in data_loader_val:
-                output = model(images)
-                _, predicted = torch.max(output.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        accuracy = 100 * correct / total
-        print(f"Accuracy on validation set: {accuracy}%")
+        # Rest of the code remains the same
 
-    return model
+
+
 
 
 def main():
-    if os.path.isfile('../pth/model.pth'):
-        model = NeuralNetwork(10)
-        model.load_state_dict(torch.load('../pth/model.pth'))
-    else:
-        model = NeuralNetwork(10)
-        
+    torch.set_default_tensor_type(torch.cuda.HalfTensor if torch.cuda.is_available() else torch.FloatTensor)
+    model = NeuralNetwork()
+    dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
+
+    model = train(model, data_loader_train, data_loader_val)
+
+    torch.save(model.state_dict(), '../pth/model.pth')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
+
     return model
+
+
 
 
 
